b72f82dda555562d86b4d535842c88526873f455
inajob <ina_job@hotmail.com>
2022-11-05 06:53:58 +0900

    move cursor up/down

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,14 +4,32 @@ import Line from './Line'
 class Editor extends React.Component{
 	constructor(props){
 		super(props);
-		this.state = {lines: props.lines};
+		this.state = {
+			lines: props.lines,
+			cursor: {row: 0, col: 0},
+		};
 		this.handleChange = this.handleChange.bind(this);
+		this.onKeyDown = this.onKeyDown.bind(this);
 	}
 	handleChange(i){
-		return (event) => {
-			console.log(this.state)
-		  this.state.lines[i] = event.target.value;
-		  this.setState({lines: this.state.lines});
+		return (e) => {
+		  this.state.lines[i] = e.target.value;
+		  this.setState(this.state);
+		}
+	}
+	onKeyDown(i){
+    return (e) => {
+			switch(e.keyCode){
+				case 38: // up
+					this.state.cursor.row --;
+		      this.setState(this.state);
+					break;
+				case 40: // down
+					this.state.cursor.row ++;
+		      this.setState(this.state);
+					break;
+				default:
+			}
 		}
 	}
 
@@ -19,7 +37,13 @@ class Editor extends React.Component{
 		return (
 			<div>
         {this.props.lines.map((line, index) => (
-					<Line key={index} value={line} onChange={this.handleChange(index)} />
+					<Line
+					  key={index}
+				    value={line}
+					  isFocus={index===this.state.cursor.row}
+					  onChange={this.handleChange(index)}
+					  onKeyDown={this.onKeyDown(index)}
+					/>
         ))}
 			</div>
 		)
--- src/components/Line.js
+++ src/components/Line.js
@@ -3,9 +3,26 @@ import React from 'react';
 class Line extends React.Component{
   render() {
 		return (
-			<textarea value={this.props.value} onChange={this.props.onChange} />
+			<textarea
+			  ref="rawInput"
+			  value={this.props.value}
+			  onChange={this.props.onChange}
+			  onKeyDown={this.props.onKeyDown}
+			/>
 		)
 	}
+  updateFocus(){
+    if(this.props.isFocus){
+      this.refs.rawInput.focus();
+    }
+  }
+  componentDidUpdate(){
+    this.updateFocus()
+  }
+  componentDidMount(){
+    this.updateFocus()
+  }
+
 }
 
 export default Line
