789b18281d8df8d5cd4238c757151675bd8722d7
inajob <ina_job@hotmail.com>
2022-11-05 14:09:37 +0900

    implements Enter, Backspace

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -21,17 +21,44 @@ export const Editor = (props) => {
               return [...prev];
             })
           })(index)}
-          onKeyDown={(e) => setCursor((prev) => {
-            switch(e.key) {
-              case "ArrowUp":
-                return { row: prev.row - 1, col: prev.col };
-              case "ArrowDown":
-                return { row: prev.row + 1, col: prev.col };
-              default:
-                // 同じobjectを返せば再レンダリングされない
-                return prev;
-            }
-          })}
+          onKeyDown={(e) => {
+            setCursor((prev) => {
+              switch(e.key) {
+                case "ArrowUp":
+                  if(prev.row === 0)return prev;
+                  return { row: prev.row - 1, col: prev.col };
+                case "ArrowDown":
+                  if(prev.row === lines.length - 1)return prev;
+                  return { row: prev.row + 1, col: prev.col };
+                case "Backspace":
+                  if(e.target.selectionStart === 0 && e.target.selectionEnd === 0){
+                    if(prev.row === 0)return prev;
+                    setLines((prevLines) => {
+                      // 上の行と結合する
+                      prevLines[prev.row - 1] += prevLines[prev.row];
+                      prevLines.splice(prev.row, 1);
+                      return [...prevLines];
+                    });
+                    e.preventDefault();
+                    return { row: prev.row - 1, col: prev.col };
+                  }
+                  return prev;
+                case "Enter":
+                  setLines((prevLines) => {
+                    const column = e.target.selectionStart;
+                    const afterCursor = prevLines[prev.row].slice(column);
+                    prevLines[prev.row] = prevLines[prev.row].slice(0, column);
+                    prevLines.splice(prev.row + 1, 0, afterCursor);
+                    return [...prevLines];
+                  });
+                  e.preventDefault();
+                  return { row: prev.row + 1, col: prev.col };
+                default:
+                  // 同じobjectを返せば再レンダリングされない
+                  return prev;
+              }
+            })
+          }}
         />
       ))}
     </div>);
