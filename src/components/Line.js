291130cd5fdc566171395b56c06388f6c585bf74
inajob <ina_job@hotmail.com>
2022-11-06 02:20:43 +0900

    implement inline style

--- src/components/Line.js
+++ src/components/Line.js
@@ -26,14 +26,91 @@ export const Line = (props) => {
     return clist.join(" ");
   }
 
+  const calcTextareaStyle = (isFocus) => {
+    if(isFocus)return "";
+    return "hide";
+  }
+  const calcHtmlStyle = (isFocus) => {
+    if(isFocus)return "hide";
+    return "";
+  }
+
+  // multi-target indexOf
+  // return minimum index and target
+  const capture = (body, targets, offset) => {
+    var minPos = -1;
+    var minTarget = "";
+    targets.forEach(function(target){
+      var index = body.indexOf(target, offset);
+      if(index !== -1){
+        if(minPos === -1 || minPos > index){
+          minPos = index;
+          minTarget = target;
+        }
+      }
+    });
+    return {pos: minPos, target: minTarget}
+  }
+
+  const makeLine = (body) => {
+    var pos = 0;
+    var result = []
+    while(true){
+      var cap;
+      cap = capture(body, ["http://", "https://"], pos);
+      if((cap.target === "https://" || cap.target === "http://")){
+        if(pos !== cap.pos){
+          result.push(body.slice(pos, cap.pos));
+        }
+        var endPos = capture(body, [" ","\r", "\n"], cap.pos + cap.target.length);
+        if(endPos.pos !== -1){
+          let link = body.slice(cap.pos, endPos.pos)
+          result.push(<a href={link}>{link}</a>);
+          pos = endPos.pos;
+        }else{
+          let link = body.slice(cap.pos, body.length);
+          result.push(<a href={link}>{link}</a>);
+          pos = body.length;
+          break;
+        }
+      }else{
+        result.push(body.slice(pos, body.length))
+        pos = body.length;
+        break;
+      }
+    }
+    return result
+  }
+  const makeHtml = (s) => {
+    let clist = ["line"];
+    if(s.indexOf("# ") === 0){
+      clist.push("h1-style");
+    }else if(s.indexOf("## ") === 0){
+      clist.push("h2-style");
+    }else if(s.indexOf("### ") === 0){
+      clist.push("h3-style");
+    }
+    return (
+      <div className={clist.join(" ")}>
+        {makeLine(s)}
+      </div>
+    )
+  }
+
   return (
-    <div className={calcStyle(props.value, props.isFocus)}><textarea
+    <div
+      className={calcStyle(props.value, props.isFocus)}
+      onClick={props.onClick}
+    >
+       <textarea
+         className={calcTextareaStyle(props.isFocus)}
          ref={ref}
          value={props.value}
          onChange={props.onChange}
          onKeyDown={props.onKeyDown}
-         onClick={props.onClick}
-      /></div>
+      />
+      <div className={calcHtmlStyle(props.isFocus)}>{makeHtml(props.value)}</div>
+    </div>
   );
 };
 
